5b9d56794c7c50b402f28bf936254f8fe127928d
Alexander Liakouris <alexanliakou@gmail.com>
2026-05-28 14:18:14 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from "react-redux";
+import CreateCustomer from "./features/customers/CreateCustomer"
+import Customer from "./features/customers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
+
+const App = () => {
+
+  const fullName = useSelector((store) => store.customer.fullName);
+
+  return (
+    <div>
+      <h1>🏦 BusyBank</h1>
+      {
+        fullName ==='' ? (
+          <CreateCustomer />
+        ) : (
+          <>
+            <Customer />
+            <AccountOperations />
+            <BalanceDisplay />
+          </>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
