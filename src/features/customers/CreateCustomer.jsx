f1af945bafc7809be05ca4ff5a3046869ecca7a2
Alexander Liakouris <alexanliakou@gmail.com>
2026-05-29 14:59:13 +0300

    added validation in create customer fullname input

--- src/features/customers/CreateCustomer.jsx
+++ src/features/customers/CreateCustomer.jsx
@@ -5,7 +5,9 @@ import { createCustomer } from "./customerSlice";
 const Customer = () => {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
+  const [error, setError] = useState(false);
   const dispatch = useDispatch();
+  const nameRegex = /[^a-zA-Z\s-]/g;
 
  const handleClick = () => {
   if (!fullName || !nationalId) return;
@@ -19,10 +21,16 @@ const Customer = () => {
         <div className="flex flex-col gap-3">
           <label className="text-white font-bold sm:w-100">Customer full name:</label>
           <input
-          className="border p-2 font-normal text-white border-white rounded-md outline-0"
+            className="border p-2 font-normal text-white border-white rounded-md outline-0 placeholder:italic"
             value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
+            placeholder="i.e. John"
+            onChange={(e) => {
+              setError(nameRegex.test(e.target.value));
+              setFullName(e.target.value);
+            }
+          } 
           />
+          {error != "" && <p className="text-red-500 text-sm font-bold italic">Please use only letters, whitespace or '-'</p>}
         </div>
         <div className="flex flex-col gap-3">
           <label className="text-white font-bold sm:w-100">National ID:</label>
@@ -32,7 +40,7 @@ const Customer = () => {
             onChange={(e) => setNationalId(e.target.value)}
           />
         </div>
-        <button className="rounded-md bg-gray-400 text-white font-bold p-3 cursor-pointer hover:opacity-75 disabled:opacity-25" disabled={fullName  === '' || nationalId  === ''} onClick={handleClick}>Create new customer</button>
+        <button className="rounded-md bg-gray-400 text-white font-bold p-3 cursor-pointer hover:opacity-75 disabled:opacity-25" disabled={fullName  === '' || nationalId  === '' || error} onClick={handleClick}>Create new customer</button>
       </div>
     </div>
   );
